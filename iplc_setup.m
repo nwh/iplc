ecad72238433d8b6ad0cb62f68576a1494b1af43
Nick Henderson <nwh@stanford.edu>
2014-05-12 16:19:18 -0700

    update iplc_setup

--- iplc_setup.m
+++ iplc_setup.m
@@ -1,2 +1,25 @@
-load_ipopt
-load_mcute
+function iplc_setup(install)
+
+if nargin == 1 && strcmp(install,'install')
+  % get userpath directory
+  mypath = strsplit(userpath,':');
+  mypath = mypath{1};
+  % create directory if it does not exist
+  % TODO: make this better
+  [~,~,~] = mkdir(mypath)
+  % open file
+  fid = fopen([mypath '/iplc_load.m'],'w');
+  % write file
+  fprintf(fid,'function load_arcopt\n');
+  fprintf(fid,'  addpath(''%s'')\n',pwd);
+  fprintf(fid,'  addpath(''%s'')\n',[pwd '/toolbox/ipopt']);
+  fprintf(fid,'end\n');
+  % close file
+  fclose(fid);
+  %keyboard
+else
+  addpath(pwd);
+  addpath([pwd '/toolbox/ipopt'])
+end
+
+end
--- load_iplc.m
+++
@@ -1,8 +0,0 @@
-%load_iplc  load script for iplc
-
-function load_iplc
-  
-  addpath([getenv('HOME') '/Dropbox/code/ipopt/lib']);
-  addpath([getenv('HOME') '/Dropbox/code/iplc']);
-  
-end
